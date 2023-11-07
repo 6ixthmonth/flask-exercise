051791240248ae50ab5e2c447928eb16ddb55ecc
June <42332051+6ixthmonth@users.noreply.github.com>
2023-11-07 19:13:42 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -11,6 +11,13 @@ def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
 if __name__ == '__main__':
     # app.debug = True
     # app.run()
