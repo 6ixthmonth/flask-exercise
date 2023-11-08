7a46ff9aad61804fdac6deb23281967c8db6bedf
June <42332051+6ixthmonth@users.noreply.github.com>
2023-11-08 19:14:01 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,24 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        do_the_login()
-    else:
-        show_the_login_form()
-
-if __name__ == '__main__':
-    # app.debug = True
-    # app.run()
-    app.run(debug=True)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
