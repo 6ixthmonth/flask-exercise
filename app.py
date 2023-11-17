3b6e9008593ef3eab8dc0e07c4478b7f6944ac50
June <42332051+6ixthmonth@users.noreply.github.com>
2023-11-17 19:26:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -64,3 +64,13 @@ with app.test_request_context():
     print(url_for('login'))
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
+
+# from flask import request
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+    
