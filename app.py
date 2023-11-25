f5bea8fb06c1487105cb5c612b86d20fee25c0f3
June <42332051+6ixthmonth@users.noreply.github.com>
2023-11-25 19:43:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from flask import request
-from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
